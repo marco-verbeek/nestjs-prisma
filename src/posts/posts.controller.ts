76e493449aa74f5aa1129de2a4c6e1c635bafe17
Marco <marcoverbeek99@gmail.com>
2021-10-09 17:34:35 +0200

    Implemented Posts controller

--- src/posts/posts.controller.ts
+++ src/posts/posts.controller.ts
@@ -1,4 +1,45 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post } from '@nestjs/common';
+import { PostsService } from './posts.service';
+import { Post as PostModel } from '@prisma/client';
 
 @Controller('posts')
-export class PostsController {}
+export class PostsController {
+  constructor(private readonly postsService: PostsService) {}
+
+  @Get(':id')
+  async getPostById(@Param('id') id: string): Promise<PostModel> {
+    return this.postsService.post({ id: Number(id) });
+  }
+
+  @Get('feed')
+  async getPublishedPosts(): Promise<PostModel[]> {
+    return this.postsService.posts({ where: { published: true } });
+  }
+
+  @Get('search/:searchString')
+  async getSearchedPosts(
+    @Param('searchString') searchString: string,
+  ): Promise<PostModel[]> {
+    return this.postsService.posts({
+      where: {
+        OR: [
+          { title: { contains: searchString } },
+          { content: { contains: searchString } },
+        ],
+      },
+    });
+  }
+
+  @Post()
+  async createDraft(
+    @Body() postData: { title: string; content?: string; authorEmail: string },
+  ): Promise<PostModel> {
+    const { title, content, authorEmail } = postData;
+
+    return this.postsService.createPost({
+      title,
+      content,
+      author: { connect: { email: authorEmail } },
+    });
+  }
+}
