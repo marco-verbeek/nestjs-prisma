0d7ac063c72b2067418bd5cf33e740c31bf27495
Marco <marcoverbeek99@gmail.com>
2021-10-09 21:16:29 +0200

    Added and implemented OpenAPI dependency

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,5 @@
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 import { PrismaService } from './prisma.service';
 
@@ -7,6 +8,14 @@ async function bootstrap() {
 
   const prismaService: PrismaService = app.get(PrismaService);
   prismaService.enableShutdownHooks(app);
+
+  const swaggerConfig = new DocumentBuilder()
+    .setTitle('NestJS + Prisma')
+    .setDescription('Small project integrating Prisma in NestJS.')
+    .setVersion('1.0')
+    .build();
+  const swaggerDocument = SwaggerModule.createDocument(app, swaggerConfig);
+  SwaggerModule.setup('api', app, swaggerDocument);
 
   await app.listen(3000);
 }
